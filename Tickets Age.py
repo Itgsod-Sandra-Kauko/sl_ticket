d0f639c9932bfe82803783d609f6e8019a9162af
Sandra <32324637+Itgsod-Sandra-Kauko@users.noreply.github.com>
2017-10-17 10:59:23 +0200

    Add files via upload

---
+++ Tickets Age.py
@@ -0,0 +1,16 @@
+# asks for an age
+# prints the price
+
+Price = input('Welcome. Whats your age?')
+
+if Price > 21:
+    print Price,'years = 30kr'
+
+elif Price > 11:
+    print Price, 'years = 15kr'
+
+elif Price < 12:
+    print Price,'years = Free(0kr)'
+
+
+
---
+++ Tickets Status.py
@@ -0,0 +1,16 @@
+Status = raw_input('Welcome. Whats your status?')
+
+if Status == ("Student"):
+                  print Status, '= 20kr'
+
+elif Status == ("Senior"):
+    print Status, '= 15kr'
+
+elif Status == ('Teen'):
+    print Status, '= 15kr'
+
+elif Status == ('Adult'):
+    print Status, '= 30kr'
+
+elif Status == ('Child'):
+    print Status, '= Free(0kr)'
